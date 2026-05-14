495ceb608f011c95d6446a2d27e02de19b90085c
Kelso <koasobrinho2008@gmail.com>
2026-05-14 09:46:47 -0300

    fix: Consertando erro no js

--- src/js/script.js
+++ src/js/script.js
@@ -1,70 +1,67 @@
-// Declarações dos elementos usando DOM(Document Object Model)
-const videoElemento = document.getElementById('video');
-const botaoScanear = document.getElementById('btn-texto');
-const resultado = document.getElementById('saida');
-const canvas = document.getElementById('canvas');
+//Declarações dos Elementos usando DOM(Document Object Model)
+const videoElemento = document.getElementById("video");
+const botaoScanear = document.getElementById("btn-texto");
+const resultado = document.getElementById("saida");
+const canvas = document.getElementById("canvas");
 
-
-// Função assincrona para habilitar a câmera
-
-async function configurarCamera() {
-    // Tratamento de Erros
-    try {
-        // Chama a API do navegador para solicitar acesso
-        const midia = await navigator.mediaDevices.getUserMedia({
-            // Habilita a câmera traseira
-            video:{facingMode: 'environment'},
-            // O áudio não será capturado
-            audio: false
+//Função assincrona para habilitar a câmera
+async function configurarCamera(){
+    //tratamento de erros
+    try{
+        //chama a api do navegador para solicitar acesso
+        const midia= await navigator.mediaDevices.getUserMedia({
+            //habilit a câmera traseira
+            video:{ facingMode: "environment"},
+            //o audio não será capturado
+            audio:false
         });
-        // recebe a função midia para ser executada
+        //recebe a função midia para ser executada
         videoElemento.srcObject=midia;
-        // força a reprodução do vídeo
+        //força a reprodução do video
         videoElemento.play();
 
-
-    } catch (erro){
-        resultado.innerText = "erro ao acessar a câmera", erro;
+    }catch(erro){
+        resultado.innerText="Erro ao acessar a Câmera",erro;
     }
-    
 }
-// Executando a função
+//executando a função
 configurarCamera();
 
-// Função para capturar o texto da câmera
-botaoScanear.onclick = async ()=>{
-    // estou habilitando a camera
-    botaoScanear.disabled = true;
-    resultado.innerText = 'Fazendo a leitura do texto... aguarde'
-    
-    // Define o canvas para iniciar a leitura
-    const contexto = canvas.getContext('2d');
+//função para capturar o texto da câmera
+botaoScanear.onclick =async ()=>{
+     botaoScanear.disabled=true; // habilitando a camera
+     resultado.innerText="Fazendo a leitura do texto...aguarde";
 
-    // Ajusta o tamanho do canvas para o tamanho real do vídeo
-    canvas.width = videoElemento.videoWidth;
-    canvas.height = videoElemento.videoHeight;
+     //Define o canvas para iniciar a leitura
+     const contexto = canvas.getContext("2d");
 
-    // Aplica o filtro para melhorar o OCR
-    contexto.filter = 'contraste(1.2) grayscale(1)'
+     //ajusta o tamanho do canvas para o tamanho real do video
+     canvas.width =videoElemento.videoWidth;
+     canvas.height = videoElemento.videoHeight;
 
-    // Desenha o video no canvas
-    contexto.drawImage(videoElemento,0,0,canvas.width,canvas.height)
+     //aplica o filtro para melhorar o OCR
+     contexto.filter='contrast(1.2) grayscale(1)';
 
-    try {
-        const {data:{texto}} = await Tesseract.recognize(
+     //desenha o video no canvas
+
+     contexto.drawImage(videoElemento,0,0,canvas.width,canvas.height);
+
+     try{
+        const {data:{ text }}=await Tesseract.recognize(
             canvas,
-            'por' //Define o idioma
+            'por' //define o idioma
         );
-        // remove os espaços em brancos
-        const textoFinal = texto.trim();
-        // Estrutura condicional ternaria (If = ? | Else = :)
-        resultado.innerText = textoFinal.lenght > 0? textoFinal: 'Não foi possível identificar o texto';
+        //remove os espaços em branco
+        const textoFinal = text.trim();
+        //estrutura condicional ternaria ? = if : =else
+        resultado.innerText=textoFinal.length > 0 ?textoFinal: "Não foi possivel identificar o texto";
+        
+     }catch(erro){
+        resultado.innerText="Erro no processamento",erro
+     }
+     finally{ 
+        //desabilita o botão para fazer nova captura
+            botaoScanear.disabled=false; 
+     }
 
-    } catch (erro) {
-        resultado.innerText = 'Erro no processamento',erro
-    }
-    finally{
-        // Desabilita o botão para fazer uma nova captura
-            botaoScanear.disabled = false;
-    }
 }
